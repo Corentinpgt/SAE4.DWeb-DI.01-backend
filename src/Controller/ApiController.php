08b4f18cdd6f6b7570c38831c2fddfe3b4140fab
Corentinpgt <corentin.pouget.19@gmail.com>
2024-03-18 10:45:59 +0100

    y

---
+++ src/Controller/ApiController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use App\Entity\Movie;
+use App\Entity\Category;
+
+class ApiController extends AbstractController
+{
+    #[Route('/api/movie/{id}', name: 'app_api_movie')]
+    public function readMovie(Movie $mov, SerializerInterface $serializer): Response
+    {
+        $data = $serializer->normalize($mov, null, ['groups' => 'json_movie']);
+        $response = new JsonResponse($data);
+        return $response;
+    }
+
+    #[Route('/api/category/{id}', name: 'app_api_category')]
+    public function readCategory(Category $cat, SerializerInterface $serializer): Response
+    {
+        $data = $serializer->normalize($cat, null, ['groups' => 'json_category']);
+        $response = new JsonResponse($data);
+        return $response;
+    }
+}
